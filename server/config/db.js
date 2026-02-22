7bc8b8c61dba2373e8e2b18a0193167b3b1195ca
Suryasekhar Dhal <suryasekhardhal2003@gmail.com>
2026-02-22 19:48:24 +0530

    add configuration files for Prettier and MongoDB connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant";
 
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `Database Connected!!DB HOST ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection error");
+    process.exit(1);
+  }
+};
+
+export default connectDB;
