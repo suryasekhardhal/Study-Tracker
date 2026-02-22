2af9746b2bbab297e0adf7e99d97705749f4178e
Suryasekhar Dhal <suryasekhardhal2003@gmail.com>
2026-02-22 20:00:54 +0530

    initialize server with Express, CORS, and cookie-parser setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import connectDB from "./config/db";
+import { app } from "./app";
+import dotenv from "dotenv";
+
+dotenv.config({path:'./.env'});
+
+connectDB()
+.then(() => {
+   app.listen(process.env.PORT, () => {
+     console.log(`Server is running on port ${process.env.PORT}`);
+   });
+})
+.catch((error) => {
+  console.error("Database connection failed:", error);
+});
